9f71bee61d16717707043f741467265b9e94fcec
bechirjlidi <78954664+bechirjlidi@users.noreply.github.com>
2022-10-17 19:00:19 +0100

    CrudAbsence

---
+++ src/main/java/com/jmlessous/controllers/AbsenceController.java
@@ -0,0 +1,61 @@
+package com.jmlessous.controllers;
+
+import com.jmlessous.entities.Absence;
+import com.jmlessous.services.IAbsenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Absence")
+public class AbsenceController {
+    @Autowired
+    IAbsenceService abs;
+    //http://localhost:8083/JMLessous/Absence/retrieve-all-Absence
+    @GetMapping("/retrieve-all-Absence")
+    @ResponseBody
+    public List<Absence> getAbsence() {
+        List<Absence> listcom = abs.retrieveAllAbsences();
+        return listcom;
+    }
+    //http://localhost:8083/JMLessous/Absence/add-Absence/{client-id}
+    @PostMapping("/add-Absence/{client-id}")
+    @ResponseBody
+    public Absence addComplaint(@RequestBody Absence c ,@PathVariable("client-id") Long clientid)
+    {
+        Absence absence = abs.addOffre(c,clientid);
+        return absence;
+    }
+    //http://localhost:8083/JMLessous/Absence/retrieve-all-Absence/{client-id}
+    @GetMapping("/retrieve-all-Absence/{client-id}")
+    @ResponseBody
+    public List<Absence> getAbsencebyuser(@PathVariable("client-id") Long clientid) {
+        List<Absence> listabs = abs.retrieveAbsenceByUser(clientid);
+        return listabs;
+    }
+
+    //http://localhost:8083/JMLessous/Absence/update-Absence/{client-id}
+    @PutMapping("/update-Absence/{client-id}")
+    @ResponseBody
+    public Absence updateComplaint(@RequestBody Absence c ,@PathVariable("client-id") Long clientid)
+    {
+        Absence absence = abs.updateAbsence(c,clientid);
+        return absence;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/jmlessous/services/AbsenceServiceImpl.java
+++ src/main/java/com/jmlessous/services/AbsenceServiceImpl.java
@@ -1,4 +1,45 @@
 package com.jmlessous.services;
 
-public class AbsenceServiceImpl {
+import com.jmlessous.entities.Absence;
+import com.jmlessous.entities.Utilisateur;
+import com.jmlessous.repositories.AbsenceRepository;
+import com.jmlessous.repositories.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AbsenceServiceImpl implements IAbsenceService{
+    @Autowired
+    AbsenceRepository absenceRepo;
+    UtilisateurRepository utilisateurRepo;
+
+
+    @Override
+    public List<Absence> retrieveAllAbsences() {
+        return (List<Absence>) absenceRepo.findAll();
+    }
+
+    @Override
+    public Absence addOffre(Absence a, Long id) {
+        Utilisateur u = utilisateurRepo.findById(id).orElse(null);
+        a.setUtilisateursAbsence(u);
+
+        return absenceRepo.save(a);
+    }
+
+    @Override
+    public List<Absence> retrieveAbsenceByUser(Long id_user) {
+        return absenceRepo.getAbsenceByUser(id_user);
+    }
+
+    @Override
+    public Absence updateAbsence(Absence a, Long id_user) {
+        Utilisateur u = utilisateurRepo.findById(id_user).orElse(null);
+        a.setUtilisateursAbsence(u);
+
+        return absenceRepo.save(a);
+
+    }
 }
