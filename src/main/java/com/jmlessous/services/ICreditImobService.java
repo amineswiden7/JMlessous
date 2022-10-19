ae2185b06929e62ed71f3c582e8a0bc66e7ceaed
bechirjlidi <78954664+bechirjlidi@users.noreply.github.com>
2022-10-19 13:21:04 +0100

    Update ICreditImobService.java

--- src/main/java/com/jmlessous/services/ICreditImobService.java
+++ src/main/java/com/jmlessous/services/ICreditImobService.java
@@ -1,4 +1,24 @@
 package com.jmlessous.services;
 
+import com.jmlessous.entities.Absence;
+import com.jmlessous.entities.CreditImmobilier;
+
+import java.util.List;
+
 public interface ICreditImobService {
+    List<CreditImmobilier> retrieveAllCredit();
+
+    CreditImmobilier addCredit(Absence a , Long id_Compte,Long id_garantie);
+
+    List<Absence> retrieveAbsenceByUser(Long id_user);
+
+    Absence updateAbsence(Absence a , Long id_user);
+
+
+
+
+
+
+
+
 }
